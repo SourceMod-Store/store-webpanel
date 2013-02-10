b07770a92165e91c89a4d4127aa39ed979185ba1
Arrow768 <arrow768@sourcedonates.com>
2013-02-10 23:23:26 +0100

    Chagned the HTML Pages to CI View Files

--- application/controllers/categories.php
+++ application/controllers/categories.php
@@ -1,27 +1,26 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
-	}
+class Categories extends CI_Controller {
+    
+    public function index()
+    {
+        $this->load->view('parts/header');
+        $this->load->view('pages/categories/manage');
+        $this->load->view('parts/footer');
+    }
+    
+    public function add(){
+        $this->load->view('parts/header');
+        $this->load->view('pages/categories/add');
+        $this->load->view('parts/footer');
+    }
+    
+    public function edit($slug){
+        $this->load->view('parts/header');
+        $this->load->view('pages/categories/edit');
+        $this->load->view('parts/footer');
+    }
 }
 
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
+/* End of file categories.php */
+/* Location: ./application/controllers/categories.php */
