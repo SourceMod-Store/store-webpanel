20072235fd1d9443b8094767403c23fad1af3dd9
John Lewis <jljr222@gmail.com>
2013-02-26 17:58:32 -0500

    Fixed various incorrect HTML code

--- application/views/parts/footer.php
+++ application/views/parts/footer.php
@@ -1,12 +1,11 @@
   <hr>
   <footer>
-    <center>
     <p>&copy; 2013 Store Plugin Developed by <strong>alongub</strong>. Code by <a href="http://www.sourcedonates.com/" target="_blank"><strong>Arrow768</strong></a>. Design by <a href="http://www.bor3dgaming.com/" target="_blank"><strong>Bor3d Gaming</strong></a>.
-    <p>WebPanel Version 0.3.1. Go to <a href="http://jira.sourcedonates.com/browse/STWPAN" target="_blank"><strong>Jira</strong></a> to report Bugs and request Features.</p></p>
-    </center>
+    <p>WebPanel Version 0.3.1. Go to <a href="http://jira.sourcedonates.com/browse/STWPAN" target="_blank"><strong>Jira</strong></a> to report Bugs and request Features.</p>
   </footer>
 </div>
-<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>--> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/tablesorter/jquery.tablesorter.js");?>"></script> 
 <script src="<?php echo base_url("assets/js/bootstrap.min.js")?>"></script> 
 <script src="<?php echo base_url("assets/js/main.js")?>"></script>
 </body>
