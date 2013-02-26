b54513104d59a6526a9cc5ff944016311af3944b
John Lewis <jljr222@gmail.com>
2013-02-26 17:59:08 -0500

    Update application/views/parts/footer.php

--- application/views/parts/footer.php
+++ application/views/parts/footer.php
@@ -5,7 +5,7 @@
   </footer>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url("assets/tablesorter/jquery.tablesorter.js");?>"></script> 
+<script src="<?php echo base_url("assets/tablesorter/jquery.tablesorter.js");?>"></script> 
 <script src="<?php echo base_url("assets/js/bootstrap.min.js")?>"></script> 
 <script src="<?php echo base_url("assets/js/main.js")?>"></script>
 </body>
