2cc0faf799059fbf59d1cd97ed18b5df400de317
Arrow768 <arrow768@sourcedonates.com>
2013-03-25 22:28:43 +0100

    updated a file

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = 'zkuasbdfkjnalsdfhnbkzuasgdbcklasdihjfnazskudbazuwer6zf89o32zhrbnfdilucsyx';
+$config['encryption_key'] = '';
 
 /*
 |--------------------------------------------------------------------------
