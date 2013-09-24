c9b24b6712db1928ec6702025ebd67d26c2140f1
Werner 'Arrow768 <arrow768@sourcedonates.com>
2013-09-24 20:13:17 +0200

    Prep for a automated installer

--- application/config/config.sample.php
+++ application/config/config.sample.php
@@ -227,7 +227,7 @@ $config['cache_path'] = '';
   | MUST set an encryption key.  See the user guide for info.
   |
  */
-$config['encryption_key'] = '1234test';
+$config['encryption_key'] = '[REPLACE-ENCRYPTION_KEY]';
 
 /*
   |--------------------------------------------------------------------------
